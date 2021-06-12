e626ce631ede42482ca53b095ead6374738d4713
Layra <clementine.rousseau95@gmail.com>
2021-06-12 15:43:54 +0200

    Outline Effect

---
+++ Assets/Scripts/ChainBehaviour.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChainBehaviour : MonoBehaviour
+{
+    private bool firstCollision = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // TODO : Rewrite when monkey behaviour finished
+        if (firstCollision)
+        {
+            // Find the collision parent
+            GameObject connectedGO = collision.gameObject;
+            // Add the joint
+            connectedGO.AddComponent<HingeJoint>();
+            // Configure the joint
+            HingeJoint hingeJoint = connectedGO.GetComponent<HingeJoint>();
+            hingeJoint.autoConfigureConnectedAnchor = false;
+            hingeJoint.axis = new Vector3(0,0,1);
+            hingeJoint.connectedBody = gameObject.GetComponent<Rigidbody>();
+            hingeJoint.connectedAnchor = new Vector3(0, 3.5f, 0);
+            JointSpring spring = hingeJoint.spring;
+            spring.damper = 5.0f;
+            hingeJoint.spring = spring;
+            hingeJoint.useSpring = true;
+            gameObject.GetComponent<Rigidbody>().useGravity = true;
+
+            firstCollision = false;
+        }
+    }
+}
