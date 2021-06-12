e490c12612ef3e38fd62661f8a09107185e34a87
Mathieu P <painmathie@eisti.eu>
2021-06-12 14:57:00 +0100

    Remove

--- Assets/Scripts/Monkey/MonkeyClick.cs
+++ Assets/Scripts/Monkey/MonkeyClick.cs
@@ -27,12 +27,14 @@ public class MonkeyClick : MonoBehaviour
     void Awake(){
     
         _controls = new MainControls();
-        _controls.Main.Select.started += OnMouseClick;
+        _controls.Main.Select.started += OnMouseClickLeft;
+        _controls.Main.Unselect.started += OnMouseClickRight;
     }
 
     void OnDestroy(){
     
-         _controls.Main.Select.started -= OnMouseClick;
+        _controls.Main.Select.started -= OnMouseClickLeft;
+        _controls.Main.Unselect.started -= OnMouseClickRight;
     }
 
     void OnEnable()
@@ -45,7 +47,7 @@ public class MonkeyClick : MonoBehaviour
         _controls.Disable();
     }
     // Update is called once per frame
-    void OnMouseClick(CallbackCtx ctx)
+    void OnMouseClickLeft(CallbackCtx ctx)
     {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(_controls.Main.Mouse.ReadValue<Vector2>());
@@ -53,6 +55,17 @@ public class MonkeyClick : MonoBehaviour
                     if (hit.transform.gameObject.tag == "Monkey") {
                         _playerManager.selectedMonkey = hit.transform.gameObject.GetComponent<Monkey>();
                     }
-                }
             }
     }
+    
+    void OnMouseClickRight(CallbackCtx ctx)
+    {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(_controls.Main.Mouse.ReadValue<Vector2>());
+            if (Physics.Raycast(ray, out hit, 100.0f)){
+                    if (hit.transform.gameObject.tag == "Monkey") {
+                        Destroy(hit.transform.gameObject);
+                    }
+            }
+    }
+}
