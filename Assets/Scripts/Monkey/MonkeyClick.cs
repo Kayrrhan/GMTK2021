0c8a0678c9f22190f2da7c697a47e55197aedbae
Mathieu P <painmathie@eisti.eu>
2021-06-12 12:23:11 +0100

    Better input

--- Assets/Scripts/Monkey/MonkeyClick.cs
+++ Assets/Scripts/Monkey/MonkeyClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CallbackCtx = UnityEngine.InputSystem.InputAction.CallbackContext;
 
 public class MonkeyClick : MonoBehaviour
 {
@@ -11,29 +12,49 @@ public class MonkeyClick : MonoBehaviour
 
     #endregion
 
+    #region private members
+
+    MainControls _controls = null;
+    
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    void Awake(){
+    
+        _controls = new MainControls();
+        _controls.Main.Select.started += OnMouseClick;
+    }
 
-    // Update is called once per frame
-    void Update()
+    void OnDestroy(){
+    
+         _controls.Main.Select.started -= OnMouseClick;
+    }
+
+    void OnEnable()
     {
-        if (Input.GetMouseButtonDown(0)){
+        _controls.Enable();
+    }
 
+    void OnDisable()
+    {
+        _controls.Disable();
+    }
+    // Update is called once per frame
+    void OnMouseClick(CallbackCtx ctx)
+    {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(_controls.Main.Mouse.ReadValue<Vector2>());
             if (Physics.Raycast(ray, out hit, 100.0f)){
                 if (hit.transform != null){
                     if (hit.transform.gameObject.tag == "Monkey") {
                         MonkeyMovement MonkeyMovement = _playerManager.GetComponent<MonkeyMovement>();
                         MonkeyMovement.changeMonkey(hit.transform.gameObject);
-
-                        Debug.Log(hit.transform.gameObject.name);
                     }
                 }
             }
-        }
     }
 }
