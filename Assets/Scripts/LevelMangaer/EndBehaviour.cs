ecdfd64e6e778fdef09bcace9b100f3a91aed396
josselinMorau <josselin.morau@gmail.com>
2021-06-13 17:04:32 +0200

    [WIP] create AutoMonkey script and prefab

--- Assets/Scripts/LevelMangaer/EndBehaviour.cs
+++ Assets/Scripts/LevelMangaer/EndBehaviour.cs
@@ -7,10 +7,14 @@ public class EndBehaviour : MonoBehaviour
     [SerializeField]
     LevelBehaviour currentLevelBehaviour;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        currentLevelBehaviour.MonkeyFinish();
-        Debug.Log(collision.gameObject.name);
-        Destroy(collision.gameObject);
+        AutoMonkey monkey = collider.gameObject.GetComponent<AutoMonkey>();
+        if (monkey != null)
+        {
+            currentLevelBehaviour.MonkeyFinish();
+            Debug.Log(collider.gameObject.name);
+            Destroy(collider.gameObject);
+        }
     }
 }
